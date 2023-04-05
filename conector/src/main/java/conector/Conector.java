d0c01601360733b29e09388be0ba03d1d83a43b2
Sheila C. S. Arnold <sheila.arnold@pti.org.br>
2023-04-05 16:40:37 -0300

    Callback messages from Java

--- conector/src/main/java/conector/Conector.java
+++ conector/src/main/java/conector/Conector.java
@@ -13,7 +13,14 @@ public class Conector {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var record = new ProducerRecord<>("novo_pedido", "1922;123;253,90", "1923;123;253,90");
-        producer.send(record).get();
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            //Informa que a mensagem está sendo enviada
+            System.out.println(data.topic() + " :: " + data.partition() + "/" + data.offset() + "/" + data.timestamp());
+        }).get();
     }
 
     private static Properties properties(){
